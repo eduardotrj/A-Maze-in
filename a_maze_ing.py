70bc4a4aa88fa043559182867b728512a2b3b114
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-07 12:30:25 +0200

    feat:
    Generate Main class to work and set the structure.

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,10 +1,22 @@
 
+# Example import:
+# from Folder.file import Class
+
+#   from Graphic.window import Window
+#   from Config.loader import ...
+#   from Maze.model import
+#   from Maze.solver import
+#   from Algorithms.Generation.recursive_backtracker import 
+
 
 class MazeApplication:
 
     def __init__(self, settings) -> None:
 
         self.settings = settings
+
+        # instance components
+        # self.something = Something()
 
         self.grid = None
         self.generator = None
@@ -14,6 +26,7 @@ class MazeApplication:
         self.renderer = None
 
     def initialize(self) -> None:
+        """ Initialize the different classes to generate data """
 
         # Create the grid to work
         self.create_grid()
@@ -29,34 +42,32 @@ class MazeApplication:
 
         self.create_renderer()
 
-    #def create_grid(self):
-
-    #    self.grid = Grid(
-    #        self.settings.width,
-    #        self.settings.height
-    #    )
+    #   def create_grid(self):
+    #       self.grid = Grid(
+    #           self.settings.width,
+    #           self.settings.height
+    #       )
 
     def run(self) -> None:
+        """ Execute the functions """
 
         self.initialize()
 
         #   self.generate_maze()
+        #   self.graphic.run()
 
-
-
+    def check_settings(self) -> None:
+        """ Only for testing """
+        print("Test")
 
 
 def main() -> None:
-
     #   Eray Input
     #   settings = load_settings("settings.json")
-
     app = MazeApplication(settings)
 
-    #   app.run()
+    app.run()
 
 
 if __name__ == "__main__":
     main()
-
-
