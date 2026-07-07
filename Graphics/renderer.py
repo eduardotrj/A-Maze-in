f72a06b91fe19085d73be30e8bb649888e4c6337
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-07 16:29:12 +0200

    feat(Graphics/renderer):
    Basic structure for class renderer.

---
+++ Graphics/renderer.py
@@ -0,0 +1,25 @@
+
+
+#   renderer.draw(grid)
+class Renderer():
+    """
+    Convert data into Graphics
+    
+    
+    """
+
+    def __init__(self, window) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+    def draw_grid(self) -> None:
+        pass
+
+    def draw_cell(self, cell) -> None:
+        pass
+
+    def draw_solution(self, path) -> None:
+        pass
+
