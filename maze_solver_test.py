b306ecdbf820544f88ca3be0403355c46fc2cb68
ecakiray <eraycakiray@gmail.com>
2026-07-09 16:53:14 +0200

    one algorithm; depth-first search is added

--- maze_solver_test.py
+++ maze_solver_test.py
@@ -18,12 +18,19 @@ raw_maze = ["939551553",
 
 raw_start = (4, 2)
 raw_end = (6, 2)
+raw_solutuon = "NWSSWNWSWSSSSSENESSWSWSSENENENESEENEENNNNNNNWNWS"
 
 
 def main() -> None:
 
     maze = parse_maze(raw_maze)
     print(maze)
+    solution = solve_dfs(maze, raw_start, raw_end)
+    print(solution)
+    if raw_solutuon == solution:
+        print("It works!!")
+    else:
+        print("Please try again :(")
 
 
 if __name__ == "__main__":
