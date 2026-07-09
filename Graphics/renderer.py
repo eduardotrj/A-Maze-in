5e1cc794658f67808a0a3575c7c8b0680dfa4174
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-09 23:57:02 +0200

    feat:
    Update naming in file.

--- Graphics/renderer.py
+++ Graphics/renderer.py
@@ -1,6 +1,6 @@
-from Graphics.canvas import MLXCanvas
 from abc import ABC, abstractmethod
 import numpy as np
+import cv2
 
 # Working process:
 # Renderer -> Canvas -> MiniLibx
@@ -33,23 +33,46 @@ class Renderer(ABC):
     def draw_solution(self, path) -> None:
         pass
 
+
 class MazeRenderer(Renderer):
     """
     Render a Maze
     """
-    def __init__(self, window, size: int = 32) -> None:
+    def __init__(self, window, canvas, size: int = 32) -> None:
         self.window = window
-        self.tile_size = size  # Pixel size
-        self.canvas = MLXCanvas()  # To print into MiniLibx
+        self.tile_size = size   # Pixel size
+        self.canvas = canvas    # To print into MiniLibx
         self.theme = 'default'
         # Load Graphics:\
-        self.wall = self.gen_array('wall.png')
-        self.
+        self.wall = self.img_array('wall.png')
+        self.path = self.img_array('path.png')
+        self.background = self.img_array('bg.png')
+        self.start = self.img_array('start.png')
+        self.exit = self.img_array('exit.png')
+        self.mark = self.img_array('mark.png')
+        self.solve_n = self.img_array('solve_n.png')
+        self.solve_ne = self.img_array('solve_ne.png')
+        self.solve_e = self.img_array('solve_e.png')
+        self.solve_es = self.img_array('solve_es.png')
+        self.solve_s = self.img_array('solve_s.png')
+        self.solve_sw = self.img_array('solve_sw.png')
+        self.solve_w = self.img_array('solve_w.png')
+        self.solve_wn = self.img_array('solve_wn.png')
 
     def img_array(self, filename: str, resizing: bool = False) -> np.ndarray:
         """ Transform an img into a data array with BGRA channels """
+        # ?Use try catch or any way if not image? OR put DEFAULT?/
+        image = cv2.imread(f"Assets/{self.theme}/{filename}")
 
+        if image is None:
+            return np.asanyarray(None)
+        image_argb = cv2.cvtColor(image, code=cv2.COLOR_BGR2BGRA)
 
+        if resizing is True:
+            size = self.tile_size
+            resize_img = cv2.resize(image_argb, (size, size))
+            return np.asarray(resize_img, dtype=np.uint8)
+        return np.asarray(image_argb, dtype=np.uint8)
 
     def draw(self) -> None:
         """ Call others Draw() Methods in orden """
@@ -75,6 +98,12 @@ class MazeRenderer(Renderer):
 
         screen_x = x * self.tile_size
         screen_y = y * self.tile_size
+
+        self.canvas.draw_image(
+            self.path,
+            screen_x,
+            screen_y
+        )
 
         if cell & NORTH:
             self.canvas.draw_image(
@@ -126,4 +155,3 @@ class MazeRenderer(Renderer):
 
     def generate_points(self) -> None:
         """ Draw start and End point in the maze """
-
