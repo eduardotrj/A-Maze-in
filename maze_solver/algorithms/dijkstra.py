e929f674e2d8155070938e26eedf1913e7ac4b68
ecakiray <eraycakiray@gmail.com>
2026-07-11 23:36:48 +0200

    refactor: remove old maze solver structure

--- maze_solver/algorithms/dijkstra.py
+++
@@ -1,9 +0,0 @@
-# Dijkstra's algorithm
-# https://en.wikipedia.org/wiki/Dijkstra's_algorithm
-
-def solve_dijkstra(
-    maze: list[list[int]],
-    start: tuple[int, int],
-    end: tuple[int, int],
-) -> str | None:
-    print("Here comes the dijkstra algorithm")
