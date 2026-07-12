a4f6658e6dbad867d1edba0872f7cd6494732ad4
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-12 15:02:24 +0200

    feat: updated generator to manage different algorithms
    	modified: Maze/generator

--- Maze/generator.py
+++ Maze/generator.py
@@ -1,17 +1,36 @@
+import Algorithms.generation.recursive_backtracker as rb
+from Maze.model import Maze
 
-# from algorithms.maze_generator import
 
 class GeneratorFactory:
     """ Manage Maze generators """
+    def __init__(self, width: int, height: int, seed=None):
+        self.width = width
+        self.height = height
+        self.seed = seed
+        self.generator = rb.Backtracker(width, height, seed)
 
     @staticmethod
-    def create(name):
+    def create(name, seed=None):
+        """ Select the algorithm to generate a maze """
 
         generators = {
-
-            "prim": PrimGenerator,
-            "dfs": RecursiveBacktracker,
-            "kruskal": KruskalGenerator
+            "recursive_backtraker": rb.Backtracker,
+            #"prim": PrimGenerator,
+            #"dfs": RecursiveBacktracker,
+            #"kruskal": KruskalGenerator
         }
+        if name in generators:
+            return generators[name](seed=seed)
+        else:
+            # ? Create a default value in case not any selected???
+            raise ValueError(f"Uknown GEnerator Name {name}")
 
-        return generators[name]()
+        #return generators[name]()
+
+    @staticmethod
+    def generate_maze(width: int, height: int, seed=None):
+        """ Call Algorithm to generate a Maze """
+        generator = rb.Backtracker(width, height, seed)
+        generator.generate(width, height)
+        return generator.get_maze()
