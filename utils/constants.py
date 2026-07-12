3e7d79fe54b2f7f1d4fa883d7ae92f0428b1b19a
ecakiray <eraycakiray@gmail.com>
2026-07-12 15:01:20 +0200

    added a basic maze generator (also based on dfs) to the current config+solve structure)

--- utils/constants.py
+++ utils/constants.py
@@ -3,10 +3,18 @@ EAST = 2
 SOUTH = 4
 WEST = 8
 
+ALL_WALLS = NORTH | EAST | SOUTH | WEST
 
 DIRECTIONS = {
     "N": (0, -1, NORTH),
     "E": (1, 0, EAST),
     "S": (0, 1, SOUTH),
     "W": (-1, 0, WEST),
+}
+
+OPPOSITE_WALL = {
+    NORTH: SOUTH,
+    EAST: WEST,
+    SOUTH: NORTH,
+    WEST: EAST,
 }
