eab0fc5a19f6f5917eca842494d77b8e2da1dfe5
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-14 02:07:30 +0200

    Move outside EvenManager (Key control). Adapted multi style option.
    	modified:   a_maze_ing.py

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,9 +1,11 @@
 from Graphics.window import MLXWindow
 from Graphics.canvas import MLXCanvas
 from Graphics.renderer import MazeRenderer
+from Graphics.eventManager import EventManager
 from Maze.generator import Generator
 from Maze.model import Maze
 from mlx import Mlx
+import os
 # Example import:
 # from Folder.file import Class
 
@@ -23,35 +25,59 @@ class MazeApplication:
 
         # instance components  #! NOT HERE, Properties of Maze and renderer class.
         # self.something = Something()
-        self.width = 640
-        self.height = 900
+        self.width = 3
+        self.height = 3
         self.grid = None
+        self.grid_width = 0
+        self.grid_height = 0
         self.generator = None
         self.solver = None
-
         self.tile_size = 32
+        self.entry: tuple[int, int]
+        self.exit: tuple[int, int]
+        self.theme_index = 0
 
         # Init Graphics
         self.ve = Mlx()     # VisualEngine
+        #self.window
+        #self.canvas
+        #self.renderer
+
+    def read_confg(self) -> None:
+
+        # Data got it from reading file.
+        width_size = 6
+        height_size = 6
+
+        self.entry = (1, 1)
+        self.exit = (3, 3)
+
+        self.grid_width = width_size
+        self.grid_height = height_size
+
+        self.width = 32 * (width_size * 2 + 1)
+        self.height = 32 * (height_size * 2 + 1)
+
         self.window = MLXWindow(self.width, self.height, "A-Maze-Ing", self.ve)
-        self.canvas = MLXCanvas(self.window, self.ve, self.window.mlx,
-                                self.width, self.height, self.tile_size)
-        self.renderer = MazeRenderer(self.window, self.canvas, self.tile_size)
+        self.control = EventManager(self.ve, self.window, self)
+
+        self.get_themes()
 
     # ! TEMPORALLY UBICATED HERE --> To EvenManager
-    def hook_setup(self) -> None:
-        #self.ve.mlx_mouse_hook(self.window, on_mouse, None)
-        self.ve.mlx_key_hook(self.window.win, self.on_key, None)
-        self.ve.mlx_hook(self.window.win, 33, 0, self.on_close, None)
+    #def hook_setup(self) -> None:
+    #    #self.ve.mlx_mouse_hook(self.window, on_mouse, None)
+    #    self.ve.mlx_key_hook(self.window.win, self.on_key, None)
+    #    self.ve.mlx_hook(self.window.win, 33, 0, self.on_close, None)
 
-    def on_key(self, keynum: int, _param) -> None:
-        if keynum == 65307:
-            self.window.close()
-            self.window.end()
+    #def on_key(self, keynum: int, _param) -> None:
+    #    if keynum == 65307:
+    #        self.window.close()
+    #        self.window.end()
+    #    elif keynum == 114:
 
-    def on_close(self, _param) -> None:
-        self.window.close()
-        self.window.end()
+    #def on_close(self, _param) -> None:
+    #    self.window.close()
+    #    self.window.end()
 
         # ! ----------------------------------------
 
@@ -77,36 +103,27 @@ class MazeApplication:
     def initialize(self) -> None:
         """ Initialize the different classes to generate data """
 
-        new_maze: Maze = Generator.generate_maze(20, 20, "recursive_backtraker")
+        self.canvas = MLXCanvas(self.window, self.ve, self.window.mlx,
+                                self.width, self.height, self.tile_size)
+        self.renderer = MazeRenderer(self.window, self.canvas, self.tile_size)
+
+        self.create_maze()
+
+    def create_maze(self) -> None:
+        new_maze: Maze = Generator.generate_maze(
+            (self.grid_width * 2 + 1),
+            (self.grid_height * 2 + 1),
+            self.entry,
+            self.exit,                                              
+            "recursive_backtraker")
+        new_maze.print_values()
         self.renderer.draw(new_maze)
-
-
-
-
-        # Create the grid to work
-        #self.create_grid()
-
-        ## Create the maze data
-        #self.create_generator()
-
-        ## Create the solution
-        #self.create_solver()
-
-        ## Create window
-        #self.create_window()
-
-        #self.create_renderer()
-
-    #   def create_grid(self):
-    #       self.grid = Grid(
-    #           self.settings.width,
-    #           self.settings.height
-    #       )
 
     def run(self) -> None:
         """ Execute the functions """
 
-        self.hook_setup()
+        self.read_confg()
+        self.control.hook_setup()
         self.initialize()
         self.ve.mlx_do_sync(self.window.mlx)
         self.window.loop()
@@ -122,6 +139,20 @@ class MazeApplication:
     def check_settings(self) -> None:
         """ Only for testing """
         print("Test")
+
+    def get_themes(self):
+        cwd = os.getcwd()
+        # Get filesname
+        # filenames = next(os.walk(f"{cwd}/Assets/"), (None, None, []))[2]
+        dir_path = cwd + "/Assets/"
+        self.themes = [f for f in os.listdir(dir_path) if os.path.isdir(
+            os.path.join(dir_path, f))]
+
+    def select_theme(self):
+        self.theme_index = (self.theme_index + 1) % len(self.themes)
+        print(self.themes[self.theme_index])
+        self.renderer.new_theme(self.themes[self.theme_index])
+        #   self.renderer.theme = self.themes[self.theme_index]
 
 
 def main() -> None:
