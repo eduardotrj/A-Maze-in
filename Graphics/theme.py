f333b68ede05d1f4d7854ff1d2a3516688a58772
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-15 19:02:54 +0200

    feat:
    If any asset is missing, is generated a new one between:
    - Generate_plane_color: Generate 1 color image.
    - Generate_cellular_texture: Generate a volcanic texture.
    - Generate_brushed_texture: Generate a filament texture.
    
    	modified:   Graphics/theme.py

--- Graphics/theme.py
+++ Graphics/theme.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import sys
 import numpy as np
+import random
 import cv2
 import os
 
@@ -24,6 +26,9 @@ class ThemeManager:
         "solve_ns.png"
     ]
 
+    WIDTH = 32
+    HEIGH = 32
+
     def __init__(self, canvas, tile_size):
         self.canvas = canvas
         self.themes = {}
@@ -39,17 +44,8 @@ class ThemeManager:
             os.path.join(dir_path, f))]
         return themes
 
-
     def load_all(self):
         list_themes = self.get_list_themes()
-        #names = [
-        #    "classic",
-        #    "dungeon",
-        #    "future",
-        #    "forest",
-        #    "ice",
-        #    "lava"
-        #]
 
         for theme in list_themes:
             self.themes[theme] = {}
@@ -61,7 +57,22 @@ class ThemeManager:
                 #    )
                 #filename = Path("Assets") / theme / file
                 #filename = self.img_array(f"Assets/{theme}/{file}")
-                img_array = self.img_array(theme, file)
+
+                try:
+                    img_array = self.img_array(theme, file)
+
+                except FileNotFoundError:
+                    sys.stderr.write(f"{file} Not found. """
+                                     "Generating a random image instead")
+                    option = int(random.randint(0, 2))
+
+                    if option == 2:
+                        img_array = self.generate_brushed_texture()
+                    elif option == 1:
+                        img_array = self.generate_cellular_texture()
+                    else:
+                        img_array = self.generate_plane_color()
+
                 img = self.canvas.load_image(img_array)
 
                 # Load images in the memory only onces from here
@@ -76,7 +87,8 @@ class ThemeManager:
         image = cv2.imread(f"Assets/{theme}/{filename}")
 
         if image is None:
-            return np.asanyarray(None)
+            raise FileNotFoundError(f"Assets/{theme}/{filename}")
+            # return np.asanyarray(None)
         image_argb = cv2.cvtColor(image, code=cv2.COLOR_BGR2BGRA)
 
         if resizing is True:
@@ -90,3 +102,62 @@ class ThemeManager:
 
     def get_image(self, name):
         return self.current[name]
+
+    def generate_plane_color(self):
+        """
+            Generate random color 32x32 images.
+        """
+        image = np.zeros((self.HEIGH, self.WIDTH, 3), dtype=np.uint8)
+        b = random.randint(0, 255)
+        g = random.randint(0, 255)
+        r = random.randint(0, 255)
+
+        image[:, :, 0] = b
+        image[:, :, 1] = g
+        image[:, :, 2] = r
+
+        #cv2.imshow(name, image)
+        #cv2.imwrite(name, image)
+        image_argb = cv2.cvtColor(image, code=cv2.COLOR_BGR2BGRA)
+        return np.asarray(image_argb, dtype=np.uint8)
+
+    def generate_cellular_texture(self, num_points=100):
+        image = np.zeros((self.HEIGH, self.WIDTH), dtype=np.uint8)
+        points = np.column_stack((
+            np.random.randint(0, self.WIDTH, num_points),
+            np.random.randint(0, self.HEIGH, num_points)
+        ))
+
+        # Calculate distance from every pixel to the nearest point
+        for y in range(self.HEIGH):
+            for x in range( self.WIDTH):
+                # Calculate Euclidean distances to all point
+                distances = np.linalg.norm(points - np.array([x, y]), axis=1)
+                # Distante from closest point
+                min_dist = np.min(distances)
+
+                # Map distance to a 0-2555 grayscale range
+                val = min(int(min_dist * 5), 255)
+                image[y, x] = val
+
+        # Apply a colormap to make it colored
+        color_texture = cv2.applyColorMap(image, cv2.COLORMAP_JET)
+
+        image_argb = cv2.cvtColor(color_texture, code=cv2.COLOR_BGR2BGRA)
+        return np.asarray(image_argb, dtype=np.uint8)
+
+    def generate_brushed_texture(self):
+        # Generate fine white noise:
+        noise = np.random.randint(100, 180, (self.HEIGH, self.WIDTH), dtype=np.uint8)
+
+        # Apply horizontal motion blur kernel filter:
+        kernel_size = 30
+        kernel = np.zeros((kernel_size, kernel_size))
+        kernel[int((kernel_size - 1)/2), :] = np.ones(kernel_size)
+        kernel /= kernel_size
+
+        brushed = cv2.filter2D(noise, -1, kernel)
+
+        brushed = cv2.normalize(brushed, None, 0, 255, cv2.NORM_MINMAX)
+        image_argb = cv2.cvtColor(brushed, code=cv2.COLOR_BGR2BGRA)
+        return np.asarray(image_argb, dtype=np.uint8)
