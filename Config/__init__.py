9d2abd896320595045006f89cdb2c769737b593f
Etrujill <tuduard.edwardjig@gmail.com>
2026-07-16 19:27:16 +0200

    refactor:
    Added missed files from merge with Eray branchs
    
     Changes to be committed:
    	new file:   Config/__init__.py
    	new file:   Config/model.py
    	new file:   Config/parser.py
    	new file:   Tests/__init__.py
    	new file:   Tests/config_test.py
    	new file:   Tests/maze_generator_test.py
    	new file:   Tests/maze_solver_test.py
    	new file:   Utils/constants.py

---
+++ Config/__init__.py
@@ -0,0 +1,8 @@
+from .model import MazeConfig
+from .parser import ConfigParser
+
+
+__all__ = [
+    "ConfigParser",
+    "MazeConfig",
+]
---
+++ Tests/maze_solver_test.py
@@ -0,0 +1,76 @@
+from Algorithms import solve_bfs, solve_dfs, solve_astar, solve_dijkstra
+from maze.model import Maze
+
+# to test:
+# go to root folder and run:
+# poetry run python -m tests.maze_solver_test
+
+raw_maze = [
+    "939551553",
+    "AC693A93E",
+    "E93AE86C3",
+    "96869457A",
+    "AFAFAFFFA",
+    "AFEF857FA",
+    "AFFFAFFFA",
+    "A93FAFD52",
+    "C6AFEFFFA",
+    "B9693D152",
+    "8696C56BA",
+    "A96917946",
+    "C6D6C5457",
+]
+
+raw_solutuon = "NWSSWNWSWSSSSSENESSWSWSSENENENESEENEENNNNNNNWNWS"
+
+
+def convert_rows(lines: list[str]) -> tuple[tuple[int, ...], ...]:
+    return tuple(
+        tuple(int(char, 16) for char in line)
+        for line in lines
+    )
+
+
+def main() -> None:
+    maze = Maze(
+        rows=convert_rows(raw_maze),
+        entry=(4, 2),
+        exit=(6, 2),
+        seed=0,
+    )
+
+    solution = solve_bfs(maze)
+
+    print(solution)
+    if raw_solutuon == solution:
+        print("BFS algorithm works!!")
+    else:
+        print("Please try again :(")
+
+    solution = solve_dfs(maze)
+
+    print(solution)
+    if raw_solutuon == solution:
+        print("DFS algorithm works!!")
+    else:
+        print("Please try again :(")
+
+    solution = solve_astar(maze)
+
+    print(solution)
+    if raw_solutuon == solution:
+        print("A* algorithm works!!")
+    else:
+        print("Please try again :(")
+
+    solution = solve_dijkstra(maze)
+
+    print(solution)
+    if raw_solutuon == solution:
+        print("Dijkstra algorithm works!!")
+    else:
+        print("Please try again :(")
+
+
+if __name__ == "__main__":
+    main()
